0c666cad1959c91e005931950ad25052187c765b
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-26 11:52:47 +0300

    Step1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Header = ({ title }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header>
+      <h2>{title}</h2>
+    </header>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <main>
+      <Header title="Anna palautetta" />
+      <button onClick={() => setGood(good + 1)}>
+        Hyvä
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        Neutraali
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        Huono
+      </button>
+      <Header title="Tilastot" />
+      <p>Hyvä {good}</p>
+      <p>Neutraali {neutral}</p>
+      <p>Huono {bad}</p>
+    </main>
+  )
 }
 
 export default App;
